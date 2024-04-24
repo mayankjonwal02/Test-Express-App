3d6c5fe45260bdad8308f6d9dc3b9a0a94db3fe2
mayankjonwal02 <jonwal.1@iitj.ac.in>
2024-04-24 22:17:41 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require("cors")
+const app = express()
+const port = process.env.PORT
+
+// app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.json({message:"Hello from Mayank"})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
